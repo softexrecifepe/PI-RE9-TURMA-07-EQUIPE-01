bf18cfd17ccda84c2b13a3621b8eddcffc35a50a
Renan Menezes <renan_menezes07@hotmail.com>
2024-11-23 11:34:27 -0300

    Atualizando package.json e outros

--- src/app/navbar/page.tsx
+++ src/app/navbar/page.tsx
@@ -23,7 +23,7 @@ export function Header() {
                     <li><a>|</a></li>
                     <li><Link href="/login">Login</Link></li>
                     <li><Link href="/cadastro"><button  className="cadastro">Cadastrar CV</button></Link></li>
-                    <li><Link href="/cadastroEmpresa"><button  className="cadastro">Cadastrar Empresa</button></Link></li>
+                    <li><Link href="/cadastroEmpresa"><button className="cadastro">Cadastrar Empresa</button></Link></li>
                    </ul>
                 </div>
             </nav>
